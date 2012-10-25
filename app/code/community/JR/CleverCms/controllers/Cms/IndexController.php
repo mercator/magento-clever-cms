39c9711511f98de8ef7ed6152868a74809656869
Johann Reinke <johann.reinke@gmail.com>
2012-10-25 13:34:13 +0200

    Fixed #20 - Choose homepage in multi store mode

--- app/code/community/JR/CleverCms/controllers/Cms/IndexController.php
+++ app/code/community/JR/CleverCms/controllers/Cms/IndexController.php
@@ -7,8 +7,14 @@ class JR_CleverCms_Cms_IndexController extends Mage_Cms_IndexController
     public function indexAction($coreRoute = null)
     {
         $store = Mage::app()->getStore();
-        $pageId = Mage::getResourceModel('cms/page')->getStoreRootId($store->getId());
+        // Home page defined in config first
+        $pageId = Mage::getStoreConfig(Mage_Cms_Helper_Page::XML_PATH_HOME_PAGE);
+        if (!$pageId || !is_numeric($pageId)) {
+            // Root page of current store view
+            $pageId = Mage::getResourceModel('cms/page')->getStoreRootId($store->getId());
+        }
         if (!$pageId) {
+            // Global home page
             $pageId = Mage::getResourceModel('cms/page')->getStoreRootId(0);
         }
         if (!Mage::helper('cms/page')->renderPage($this, $pageId)) {
